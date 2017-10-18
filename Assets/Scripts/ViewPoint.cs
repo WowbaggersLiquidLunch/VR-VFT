70665204e591778dc5f0a239886510d40a100e36
jstp959 <jes418@lehigh.edu>
2017-10-18 16:45:08 -0400

    Added Classes for Objects
    Viewpoint and Flash classes

---
+++ Assets/Scripts/ViewPoint.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class ViewPoint
+{
+    private int _x;
+    private int _y;
+    private List<Flash> _flashes;
+	private int _visits;
+
+	public ViewPoint(int x, int y)
+	{
+        X = x;
+        Y = y;
+	}
+
+    public int X
+    {
+		get { return _x; }
+        set { _x = value; }
+    }
+
+    public int Y
+    {
+		get {return _y;}
+        set { _y = value; }
+    }
+
+    public List<Flash> Flashes
+    {
+		get { return _flashes; }
+        set { _flashes = value; }
+    }
+
+	public int Visits
+	{
+		get {return Flashes.Count;}
+	}
+}
+
